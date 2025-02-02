90b24e5e48d62c45f226368ca605c76bde04f67a
degrandelucas <lucasdegrande15@gmail.com>
2025-02-02 08:02:07 -0300

    zerando valor Total na opcao de limpar o carrinho

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ let listaProdutos = [];
 
 function limpar(){
     let carrinho = document.getElementById('lista-produtos');
+    let valorTotal = document.getElementById('valor-total');
     carrinho.innerHTML = '';
+    valorTotal.innerHTML = '';
     listaProdutos = [];
 }
 
